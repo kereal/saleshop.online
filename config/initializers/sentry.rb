6775a9d6e12e77ccd4281e12b6d23afe279df4c8
kereal <kereal@gmail.com>
2017-03-26 15:23:15 +0300

    - установлен sentry для логирования исключений

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,4 @@
+Raven.configure do |config|
+  config.dsn = 'https://29ca9b6d6de24c64b0db36ff7a4613e7:1dcdaf6c4f8745f6a3cb4cb06c026e8e@sentry.io/152012'
+  config.environments = %w[ production ]
+end

dfdf0b2a50fcd910b30619fb2158c2328745304d
kereal <kereal@gmail.com>
2017-04-04 13:31:40 +0300

    - email fix

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -2,8 +2,7 @@ class OrderMailer < ApplicationMailer
 
   def email_to_admin(order_id)
     @order = Order.find order_id
-    mail(to: "kereal@gmail.com", subject: "[saleshop.online] Новый заказ! (\##{@order.id})")
-    mail(to: "qvinton@gmail.com", subject: "[saleshop.online] Новый заказ! (\##{@order.id})")
+    mail(to: ["kereal@gmail.com", "qvinton@gmail.com", "skatarina.007@gmail.com"], subject: "[saleshop.online] Новый заказ! (\##{@order.id})")
   end
 
 end

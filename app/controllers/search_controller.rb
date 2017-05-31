59d9ed25759992a98e72980ddb9930fe8b9a9161
kereal <kereal@gmail.com>
2017-06-01 00:51:45 +0300

    fix

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController < ApplicationController
 
   # GET /search/:query
   def products
-    @products = Product.where("title ILIKE ?", "%#{params[:query]}%").preload(:images, :brand).to_a
+    @products = Product.where("title ILIKE ?", "%#{params[:query]}%").preload(:images, :brand).paginate(page: params[:page])
   end
 
 end

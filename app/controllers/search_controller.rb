41afc6bb1e6a94b28cc5f48230c7db29b203bc1b
kereal <kereal@gmail.com>
2017-06-01 00:45:39 +0300

    fix

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController < ApplicationController
 
   # GET /search/:query
   def products
-    @products = Product.where("title ILIKE %#{params[:query]}%")
+    @products = Product.where("title ILIKE ?", "%#{params[:query]}%")
   end
 
 end
